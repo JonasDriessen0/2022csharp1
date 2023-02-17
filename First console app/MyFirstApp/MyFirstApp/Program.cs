0073538e9e92ed4ff452acdab90b226dc68b0de6
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-02-17 11:27:47 +0100

    Zooi toevoegen

---
+++ First console app/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpOpdrachten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
