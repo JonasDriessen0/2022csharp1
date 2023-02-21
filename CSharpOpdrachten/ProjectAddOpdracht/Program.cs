b8a0c2eae764212f3d78acb8b6c7ebcfaca34f27
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-02-21 11:41:55 +0100

    Added shit back

---
+++ CSharpOpdrachten/ProjectAddOpdracht/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectAddOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dit is het programma ProjectAddOpdracht");
+        }
+    }
+}
